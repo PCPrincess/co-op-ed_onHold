c6a2d3831f14107f5f9f08b22c0c86193be96891
Lori Overholtzer <PCPrincess@outlook.com>
2017-02-12 16:53:08 -0800

    Stacked-Tab Media Box is Operational! Added a String Extensions Class and Method for Teaser Creation. Subsequent Database Migration and Update as Well.

---
+++ src/IdeallySpeaking/Data/Migrations/20170213003030_Teaser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdeallySpeaking.Data.Migrations
+{
+    public partial class Teaser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Teaser",
+                table: "Articles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Teaser",
+                table: "Articles",
+                maxLength: 240,
+                nullable: true);
+        }
+    }
+}
---
+++ src/IdeallySpeaking/Models/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeallySpeaking.Models
+{
+    public static class StringExtensions
+    {
+        public static string ArticleShortener(this string str, int length)
+        {
+            if (String.IsNullOrEmpty(str))
+                throw new ArgumentNullException(str);
+            var words = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words[0].Length > length)
+                return words[0];
+
+            var sb = new StringBuilder();
+            foreach (var word in words)
+            {
+                if ((sb + word).Length > length)
+                    return string.Format("{0}...", sb.ToString().TrimEnd(' '));
+                sb.Append(word + " ");
+            }
+            return string.Format("{0}...", sb.ToString().TrimEnd(' '));
+        }
+    }
+}
