c030e6487bf103ba3fbf813ea0383605f5ce3458
Lori Overholtzer <PCPrincess@outlook.com>
2017-03-08 16:42:44 -0800

    ApplicationUser Action-Method Work

--- src/IdeallySpeaking/Controllers/ArticlesController.cs
+++ src/IdeallySpeaking/Controllers/ArticlesController.cs
@@ -99,7 +99,7 @@ namespace IdeallySpeaking.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ArticleId,Headline,Content,Teaser,Date,AuthorUserId")] Article article)
+        public async Task<IActionResult> Edit(int id, [Bind("ArticleId,Headline,Content,Teaser,Date,Author")] Article article)
         {
             if (id != article.ArticleId)
             {
