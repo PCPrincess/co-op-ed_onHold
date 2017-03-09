357e2b3ffa3d2ab871f870d866cd368a99e51043
Lori Overholtzer <PCPrincess@outlook.com>
2017-03-09 10:46:49 -0800

    ApplicationUserController - Data Migration Comments-Profile

--- src/IdeallySpeaking/Models/ApplicationUserViewModels/ProfileViewModel.cs
+++ src/IdeallySpeaking/Models/ApplicationUserViewModels/ProfileViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace IdeallySpeaking.Models.ApplicationUserViewModels
 {
     public class ProfileViewModel
     {
+        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Last name must be alpha characters only.")]
+        [MinLength(3)]
+        [MaxLength(16)]
         public string UserName { get; set; }
 
+        [Url]
         public string Url { get; set; }
 
         public IFormFile Avatar { get; set; }
